9b427b3511eff7f6541178164daed1473c056317
shivastu Mishra <shivastumishra@gmail.com>
2026-01-30 21:16:59 +0530

    added in main.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import axios from 'axios';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const button = document.getElementById('button1');
+const output = document.getElementById('result');
 
-setupCounter(document.querySelector('#counter'))
+button.addEventListener('click', async () => {
+  try {
+    const response = await axios.get('https://backend-mify.onrender.com/api/health');
+    output.textContent = JSON.stringify(response.data, null, 2);
+  } catch (error) {
+    output.textContent = 'Error fetching data';
+    console.error(error);
+  }
+});
